b7c5c6e5db240804f4b2b11e8e2f079c00a2baa9
Santiago D. Sanchez Godoy <167319346+Zantyhago@users.noreply.github.com>
2024-04-22 10:08:47 -0300

    Update main.py

--- Unidad 2/Ejercicio 3/main.py
+++ Unidad 2/Ejercicio 3/main.py
@@ -1,8 +1,8 @@
-import GestorDeVentas from GestorDeVenta
+import GestorDeVenta from GestorDeVenta
 if __name__=='__main__':
     ns = input("Ingrese numero de semana: ")
     nd = input("Ingrese numero de dia: ")
     importe = input("Ingrese importe: ")
     nom = input("Ingrese nombre: ")
     s = input("Ingrese saldo: ")
-    Gesor1 = GestorDeVentas(n,c,ap,nom,s)
+    Gesor1 = GestorDeVenta(n,c,ap,nom,s)
