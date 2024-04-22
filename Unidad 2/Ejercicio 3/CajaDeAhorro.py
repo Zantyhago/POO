b30778f4c8eb4c0b20e2a9fdc86fd300fb13e2df
Santiago D. Sanchez Godoy <167319346+Zantyhago@users.noreply.github.com>
2024-04-22 09:27:26 -0300

    Create CajaDeAhorro.py
    Clase del jueves 18 de abril

---
+++ Unidad 2/Ejercicio 3/CajaDeAhorro.py
@@ -0,0 +1,42 @@
+class GestorDeVenta:
+    __cantDias = 7
+    __cantFarmacias = 5
+    __ventas = list [0]
+def __init__(self,):
+    for i in range (self.__cantFarmacias):
+        self.__ventas.append([0]*self.__cantDias)
+
+def Acumular(self, ns, nd, importe):
+    self.__ventas[ns][nd] += importe
+
+def Calcula(self, ns):
+    total = 0
+    for i in range(self.__cantFarmacias):
+        total += self.__ventas[ns][i]
+    return total
+
+def MayorEmision(self, nd):
+    max = 0
+    ns: int
+    for i in range(self.__cantFarmacias):
+        if max < self.__ventas[i][nd]:
+            ns = i+1
+    return ns
+
+def BuscaMenor(self, ns):
+    min = 9999999999
+    for i in range(self.__cantFarmacias):
+        acum = 0
+        for j in range(self.__cantDias):
+            acum += self.__ventas[i][j] < min
+        if acum < min:
+            min = acum
+            ns = i+1
+    return ns
+
+def AcumaTotal(self):
+    acum = 0
+    for i in range(self.__canFarmacias):
+        for j in range(self.__cantDias):
+            acum += self.__ventas[i][j]
+    return acum
