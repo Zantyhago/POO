8446d36dc4ebbecc319493409ffe110690db5b5c
Santiago D. Sanchez Godoy <167319346+Zantyhago@users.noreply.github.com>
2024-04-22 09:28:06 -0300

    Add files via upload

---
+++ Unidad 2/Ejercicio 3/main.py
@@ -0,0 +1,8 @@
+import GestorDeVentas from GestorDeVenta
+if __name__=='__main__':
+    ns = input("Ingrese numero de semana: ")
+    nd = input("Ingrese numero de dia: ")
+    importe = input("Ingrese importe: ")
+    nom = input("Ingrese nombre: ")
+    s = input("Ingrese saldo: ")
+    Gesor1 = GestorDeVentas(n,c,ap,nom,s)
