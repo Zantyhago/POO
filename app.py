10aa3199060b8590aee5033ddea329e4c39ef2c6
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2024-06-09 17:49:31 -0300

    Actualizar app.py

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def saludo():
-    return 'Mi primera aplicacion con Flask anashei /n'
+    return 'Mi primera aplicacion con Flask anashe'
+
+@app.route('/saludo_bienvenida')
+def saludoBienvenida():
+ return 'Bienvenido a mi aplicación'
+
+@app.route('/saludo_despedida')
+def saludoDespedida():
+ return 'Gracias por visitar mi aplicación Flask'
 
 if __name__ == '__main__':
     app.run()
