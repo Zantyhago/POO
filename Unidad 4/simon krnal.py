1df129d759e7ba44d4cc6d57df5a773ca7a7289d
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2024-06-17 15:40:11 -0300

    Add files via upload

--- Unidad 4/simon krnal.py
+++ Unidad 4/simon krnal.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk, messagebox
+from functools import partial
 import random
 import time
 from datetime import datetime
@@ -9,7 +10,7 @@ import json
 class Aplicacion:
     __ventana: object
     __secuencia: list
-    __marcador = IntVar
+    __marcador = int
     __mayorPtn: int
     __contador: int
     __jinicial: bool
@@ -18,24 +19,59 @@ class Aplicacion:
     __Amarillobtn: object
     __Rojobtn: object
     __Azulbtn: object
+    __Iniciarbtn: object
+    __texto: object
 
     def __init__(self):
         self.__ventana = tk.Tk()
         self.__secuencia = []
-        self.__marcador = IntVar(value = 0)
+        self.__marcador = 0
         self.__mayorPtn = 0
         self.__contador = 0
         self.__jinicial = False
-        self.__colours = ['#00DD00','#FFFF00','#FF0000','#0000FF'] # en el mismo orden: verde, amarillo,rojo, azul
+        self.__colours = ['#00DD00','#FFFF00','#FF0000','#0000FF'] # en el mismo orden; verde, amarillo,rojo, azul
         self.__ventana.title("Py-SimonGame")
-        self.__ventana.geometry("500x500")
+        self.__ventana.geometry("350x550")
         self.LauncherBotons()
         self.__ventana.mainloop()
 
     def LauncherBotons(self):
-        self.__Verdebtn = Button(self.__ventana, command = lambda: self.press("Verde"), height = 7, width = 14, bg = "green")
-        self.__Verdebtn.grid(column = 1, row=1, sticky = 'nswe', ipady=50, ipadx = 50, padx = 10, pady = 10)
-        self.__Amarillobtn = Button(self.__ventana, command = lambda: self.press("Amarillo"), height = 7, width = 14, bg = "yellow")
-        self.__Amarillobtn.place()
-        self.__Rojobtn = Button(self.__ventana, command = lambda: self.press("Rojo"), height = 7, width = 14, bg = "red")
-        self.__Azulbtn = Button(self.__ventana, command = lambda: self.press("Azul"), height = 7, width = 14, bg = "blue")
+        self.__Verdebtn = Button(self.__ventana, command = partial(self.press, "Verde"), background = self.__colours[0], relief = "raised")
+        self.__Verdebtn.place(relx = 0.265, rely = 0.3, anchor = tk.CENTER, relwidth = 0.43, relheight = 0.43)
+        self.__Amarillobtn = Button(self.__ventana, command = partial(self.press, "Verde"), background = self.__colours[1], relief = "raised")
+        self.__Amarillobtn.place(relx = 0.265, rely = 0.75, anchor = tk.CENTER, relwidth = 0.43, relheight = 0.43)
+        self.__Rojobtn = Button(self.__ventana, command = partial(self.press, "Verde"), background = self.__colours[2], relief = "raised")
+        self.__Rojobtn.place(relx = 0.735, rely = 0.3, anchor = tk.CENTER, relwidth = 0.43, relheight = 0.43)
+        self.__Azulbtn = Button(self.__ventana, command = partial(self.press, "Verde"), background = self.__colours[3], relief = "raised")
+        self.__Azulbtn.place(relx = 0.735, rely = 0.75, anchor = tk.CENTER, relwidth = 0.43, relheight = 0.43)
+        self.__ventana.grid_rowconfigure(1, weight = 1)
+        self.__ventana.grid_rowconfigure(2, weight = 1)       # para que el contenido se adapte al tamaño de la ventana
+        self.__ventana.grid_columnconfigure(0, weight = 1)
+        self.__ventana.grid_columnconfigure(1, weight = 1)
+        self.__Iniciarbtn = Button(self.__ventana, command = self.iniciar, bg = "white", text = "Iniciar", font = ('Arial', 12))
+        self.__Iniciarbtn.place(relx = 0.5, rely = 0.53, anchor = tk.CENTER, relwidth = 0.2, relheight = 0.1)
+        self.__texto = Label(self.__ventana, text = "Puntaje                         0", font = ('Arial', 15))
+        self.__texto.place(relx= 0.5, rely = 0.01,anchor = tk.N)
+
+    def press(self):
+        pass
+    
+    def iniciar(self):
+        self.__contador = 0
+        self.__marcador = 0
+        self.__secuencia = []
+        self.__jinicial = True
+        self.__texto.config(text = "Puntaje                         " + str(self.__marcador), font = ('Arial', 15))
+        self.createColor()
+        self.__Iniciarbtn.destroy()
+
+    def checkeaTurno(self):
+        pass
+
+    def createColor(self):
+        pass
+
+    def cambio(self, xboton, xcolorInic):
+        pass
+
+obj = Aplicacion()
