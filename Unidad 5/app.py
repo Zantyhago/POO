ff139fdfb6d474824104e174827fc070f5d54439
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2024-06-26 13:30:27 -0300

    Add files via upload

--- Unidad 5/app.py
+++ Unidad 5/app.py
@@ -1,1 +1,25 @@
-xd
+from flask import Flask, render_template, request, redirect, url_for, flash
+from models import db, Sucursal as sucursal, Repartidor as repartidor, Paquete as paquete, Transporte as trasnsporte
+import datetime
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+db.init_app(app)
+
+@app.route('/', methods = ['GET', 'POST'])          # 1. Acceso a la app como despachante
+def access():
+    if request.method == 'POST':
+        xidSucursal = request.form.get('Sucursal')
+        if xidSucursal:
+            idSucursal = xidSucursal
+            return redirect(url_for('gestorDespachos', idSucursal))
+    listasucursales = sucursal.query.order_by(sucursal.numero).all()    # guarda todas las sucursales de la db ordenadas por numero
+    print(f"Se cargaron {len(listasucursales)} sucursales.")        # para verificar que se cargaron correctamente
+    return render_template('sucursales.html', sucursales = listasucursales) # sucursales.html = despachantes
+
+@app.route('/sucursal/<int:idSucursal>')
+def gestorDespachos(idSucursal):
+    xsucursal = sucursal.query.get_or_404(idSucursal)   # busca en la db la sucursal, si no está devuelve un error 404
+    sucursal = xsucursal
+    return render_template('gestorDespachante.html', sucursal)
---
+++ Unidad 5/models.py
@@ -0,0 +1,43 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Paquete(db.Model):
+    __tablename__ = 'Paquete'
+    id = db.Column(db.Integer, primary_key = True)
+    numeroenvio = db.Column(db.String(50), unique = True, nullable = False)
+    peso = db.Column(db.Float, nullable = False)
+    nomdestinatario = db.Column(db.String(100), nullable = False)
+    dirdestinatario = db.Column(db.String(200), nullable = False)
+    entregado = db.Column(db.Boolean, default = False)
+    observaciones = db.Column(db.Text)
+    idsucursal = db.Column(db.Integer, db.ForeignKey('sucursal.id'), nullable = False)
+    idtransporte = db.Column(db.Integer, db.ForeignKey('Transporte.id'))
+    idrepartidor = db.Column(db.Integer, db.ForeignKey('Repartidor.id'))
+    sucursal = db.relationship('Sucursal', backref = db.backref('paquetes', lazy=True))
+    transporte = db.relationship('Transporte', backref = db.backref('paquetes', lazy=True))
+    repartidor = db.relationship('Repartidor', backref = db.backref('paquetes', lazy=True))
+
+class Repartidor(db.Model):
+    __tablename__ = 'Repartidor'
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(100), nullable = False)
+    dni = db.Column(db.String(10), unique = True, nullable = False)
+    idsucursal = db.Column(db.Integer, db.ForeignKey('Sucursal.id'), nullable = False)
+    sucursal = db.relationship('Sucursal', backref = db.backref('repartidores', lazy = True))
+
+class Sucursal(db.Model):
+    __tablename__ = 'Sucursal'
+    id = db.Column(db.Integer, primary_key = True)
+    numero = db.Column(db.String(10), unique = True, nullable = False)
+    provincia = db.Column(db.String(50), nullable = False)
+    localidad = db.Column(db.String(50), nullable = False)
+    direccion = db.Column(db.String(150), nullable = False)
+
+class Transporte(db.Model):
+    __tablename__ = 'Transporte'
+    id = db.Column(db.Integer, primary_key = True)
+    numerotransporte = db.Column(db.String(50), unique = True, nullable = False)
+    fechahorasalida = db.Column(db.DateTime, nullable = False)
+    fechahorallegada = db.Column(db.DateTime)
+    idsucursal = db.Column(db.Integer, db.ForeignKey('Sucursal.id'), nullable = False)
