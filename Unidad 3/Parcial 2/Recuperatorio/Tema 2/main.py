be9aec64e26240cb1d43b4bd9e6e81732980711c
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2024-07-01 10:55:17 -0300

    Add files via upload

---
+++ Unidad 3/Parcial 2/Recuperatorio/Tema 2/main.py
@@ -0,0 +1,9 @@
+from gestorListaPlanes import GestorPlan
+
+if __name__=='__main__':
+    GP = GestorPlan()
+    GP.leerDatos()
+    #GP.BuscaPorPosicion(912)
+    #GP.contarTiposPlanes()
+    #GP.cuentaCanalesInters()
+    GP.muestraAll()
