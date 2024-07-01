c1edee75096f922cb4459897e5d46a7ed2f4b4bb
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2024-07-01 10:55:55 -0300

    Update main.py

--- Unidad 3/Parcial 2/Recuperatorio/Tema 2/main.py
+++ Unidad 3/Parcial 2/Recuperatorio/Tema 2/main.py
@@ -3,7 +3,7 @@ from gestorListaPlanes import GestorPlan
 if __name__=='__main__':
     GP = GestorPlan()
     GP.leerDatos()
-    #GP.BuscaPorPosicion(912)
-    #GP.contarTiposPlanes()
-    #GP.cuentaCanalesInters()
+    GP.BuscaPorPosicion(912)
+    GP.contarTiposPlanes()
+    GP.cuentaCanalesInters()
     GP.muestraAll()
